499c93d1ef2bca0bfd06bb647f5a34985d38b457
Mahrad <mahradti@gmail.com>
2024-09-22 16:50:34 +0330

    RemoveToCart created

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -29,7 +29,15 @@ export const cartSlice = createSlice({
                 state.items.push({...action.payload , quantity: 1})
             }
         },
-        removeToCart(){}
+        removeToCart(state , action:PayloadAction<string>){
+            const itemIndex = state.items.findIndex((item) => item.id === action.payload)
+
+            if(state.items[itemIndex].quantity === 1){
+                state.items.splice(itemIndex, 1)
+            }else{
+               state.items[itemIndex].quantity--
+            }
+        }
     }
 })
 
