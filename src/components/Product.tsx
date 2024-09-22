40bb73d18b7987c6f1743a9c36e0351b2ba6c37e
Mahrad <mahradti@gmail.com>
2024-09-22 17:06:00 +0330

    Product FC added

---
+++ src/components/Product.tsx
@@ -0,0 +1,24 @@
+type ProductProps = {
+    id:string
+    title:string
+    price:number
+    image: string
+}
+
+
+function Product({ title , price , image}:ProductProps){
+   return (
+    <div className="product">
+         <img src={image}/>
+         <div>
+            <h3>{title}</h3>
+            <p className="product-price">{price}</p>
+         </div>
+         <p className="product-actions">
+            <button>افزودن به سبد خرید</button>
+         </p>
+    </div>
+   )
+}
+
+export default Product;
