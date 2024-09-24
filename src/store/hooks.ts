775ad5b582242aa82971d04e3d7d3d3a4eae84a9
Mahrad <mahradti@gmail.com>
2024-09-24 18:34:05 +0330

    useSelector added

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,6 +1,8 @@
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "./store";
+import { useDispatch , useSelector, type TypedUseSelectorHook } from "react-redux";
+import { AppDispatch, RootState } from "./store";
 
 type DispatchFunction = ()=> AppDispatch
 
 export const useCartDispatch: DispatchFunction = useDispatch
+
+export const useCartSelector: TypedUseSelectorHook<RootState> = useSelector
