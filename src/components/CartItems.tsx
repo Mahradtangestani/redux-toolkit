2259eeef89f14d76de9440a7808918353a8a0368
Mahrad <mahradti@gmail.com>
2024-09-25 19:17:50 +0330

    handleAddToCart added

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -1,10 +1,17 @@
-import { useCartSelector } from "../store/hooks";
+import { addToCart, type CartItem } from "../store/cart-slice";
+import { useCartDispatch, useCartSelector } from "../store/hooks";
 
 function CartItems() {
 
     const cartItems = useCartSelector((state) => state.cart.items)
 
     const totalPrice = cartItems.reduce((value, item) => value + item.price * item.quantity, 0)
+
+    const dispatch = useCartDispatch()
+
+    const handleAddToCart = (item: CartItem)=>{
+         dispatch(addToCart(item))
+    }
 
     return (
         <div>
--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -1,7 +1,7 @@
 import {createSlice, type PayloadAction} from "@reduxjs/toolkit"
 
 
-type CartItem = {
+export type CartItem = {
     id: string
     title: string
     price: number
